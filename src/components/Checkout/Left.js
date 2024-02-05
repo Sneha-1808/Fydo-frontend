8961ed059276cffeb7fbc10e949148d5c19c4d9d
Sneha Chauhan <chauhansneha22446@gmail.com>
2024-02-05 22:55:05 +0530

    new commit

--- src/components/Checkout/Left.js
+++ src/components/Checkout/Left.js
@@ -2,7 +2,7 @@ import Ticket from "./Ticket";
 
 const Left = () => {
     return (
-        <div className="flex w-full flex-col items-center justify-center text-center md:gap-6 p-[-2] sm:gap-2 md:mb-2 mb-6">
+        <div className="flex w-full flex-col items-center justify-center text-center md:gap-6 p-[-2] sm:gap-2 md:mb-2 mb-6 mt-4">
             <Ticket />
             <Ticket />
             <Ticket />
